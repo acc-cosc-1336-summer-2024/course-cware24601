d7b3d76b538e27aa3c6da0f48e38f53be63f5551
cware24601 <chris.ware@g.austincc.edu>
2024-06-23 00:51:39 +0000

    HomeWork #4

--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,21 @@
-#
+from numbers import Rational
+
+import unittest
+from src.homework.c_decisions.decisions import get_options_ratio
+from src.homework.c_decisions.decisions import get_faculty_rating
+
+class Test_Config(unittest.TestCase):
+
+    def get_options_ratio(self):
+        self.assertEqual(Rational, Test_Config())
+
+    def get_options_ratio(self):
+        ratio = get_options_ratio(5, 20)
+
+        return ratio
+
+def get_options_ratio(self):
+    ratio = get_options_ratio(10, 20)
+    return ratio        
+
+
